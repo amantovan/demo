9ec1d8d0760e40cc9db43648609bfcd2d729a0be
amantovan <89848489+amantovan@users.noreply.github.com>
2021-09-06 08:40:04 -0300

    organizando projeto

--- src/main/java/com/example/demo/controller/ClienteController.java
+++ src/main/java/com/example/demo/controller/ClienteController.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller;
 
 import com.example.demo.business.BaseBusiness;
-import com.example.demo.business.ClienteBusiness;
 import com.example.demo.model.Cliente;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
